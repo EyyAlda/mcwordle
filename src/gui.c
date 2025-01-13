a999dc89023a7c7ebf2b7d2a4223152885de7ffd
Lennard Ruetten <lennard.ruetten@gmail.com>
2025-01-13 20:51:25 +0100

    -

--- src/gui.c
+++ src/gui.c
@@ -306,7 +306,7 @@ void on_activate(GtkApplication *app, gpointer user_data){
 
 int create_user_interface(int argc, char *argv[]){
     int status;
-    GtkApplication *app = gtk_application_new("net.mcwordle", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication *app = gtk_application_new("net.mcwordle.game", G_APPLICATION_DEFAULT_FLAGS);
 
     g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
